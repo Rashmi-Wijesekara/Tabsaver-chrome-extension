4fffebf039e693f8352eaa25437dfb7c5f6302b9
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2022-01-31 20:30:33 +0530

    moved the DELETE ALL button to ALL TABS section

--- js/all-tabs.js
+++ js/all-tabs.js
@@ -1,10 +1,27 @@
+let myLeads = []
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render_Last_at_first(myLeads)
 }
+
+// Listen for double clicks on the delete button
+// When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    displayMsg("All the tabs have been deleted", 0)
+})
+
 
 // show all the tabs in the saved order
 function render(leads)
@@ -38,3 +55,37 @@ function render_Last_at_first(leads)
     ulEl.innerHTML = listItems
 }
 
+//display the message
+function displayMsg(msg, type)
+{
+    var alertmsg = document.getElementById('alert-msg')
+    alertmsg.innerHTML = msg
+
+    if(type == 0){
+        alertmsg.style.backgroundColor = "rgb(235, 140, 140)"
+        alertmsg.style.color = "white"
+    }else{
+        alertmsg.style.backgroundColor = "#c6f0ad"
+    }
+    alertmsg.style.visibility = 'visible'
+    setTimeout(hideMsg, 2000, msg)
+}
+
+//hide the message
+function hideMsg(msg)
+{
+    var alertmsg = document.getElementById('alert-msg')
+    alertmsg.innerHTML = msg
+    alertmsg.style.visibility = 'hidden'
+}
+
+// check whether the given URL is already saved before
+function checkUrl(url)
+{
+    for(let i =0; i< myLeads.length; i++){
+        if(myLeads[i].url == url){
+            displayMsg("This URL is already saved before", 0)
+            return 0
+        }
+    }
+}
